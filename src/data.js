97bef32aa8e430fa32590dca48fedb7ee6b9d238
FabiolaAboites <fasociology@gmail.com>
2019-07-10 19:29:08 -0500

    bajando cambios

--- src/data.js
+++ src/data.js
@@ -1,21 +1,22 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
 //window.example = example
 
 //descomentar para mostrar data//
 const root = document.getElementById('root')
-
-
+const btn = document.getElementById('prueba')
 const data = RICKANDMORTY.results
-let str = ''
+
+
+const printData = (data) => {
+  let str = ''
 data.forEach(element => {
   let name = element.name
   let origin = element.origin.name
   let species = element.species
   let image = element.image
-  let status = element.estatus
-  let gener = element.gener
+  let status = element.status
+  let gener = element.gender
   str +=  `<div class="fondo">
   <h3 class = "prueba">${name}</h3>
   <p class="prueba">${origin}</p>
@@ -26,11 +27,27 @@ data.forEach(element => {
 </div>`
 
 });
-
-
-
 root.innerHTML = str
+}
+
+printData(data)
 
 //llamar botones de nombre
 //function GuardarValor() {
   //document.getElementById("receive-name").value.addEventListener("click", () => document.getElementById("input-name").value);
+
+  //RICKANDMORTY.find(item =>{
+    //return (/data/g).test(item.name)
+  //})
+  //console.log(data)
+  // Selection.addEventListener)('change' , () => {
+  //  const valor = Selection.value
+  // })
+let filtrar = (data) => {
+  const result = data.filter(element => element.status == 'Dead')
+  printData(result);
+}
+
+prueba.addEventListener('click', () => {
+  filtrar(data)
+})
--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
-const filtrado = document.querySelector('#listado');
-      const boton = document.querySelector ('#boton');
-      const resultado =document.querySelector('#resultado');
+//const filtrado = document.querySelector('#listado');
+  //    const boton = document.querySelector ('#boton');
+    //  const resultado =document.querySelector('#resultado');
       //<ul id="resultado" ></ul>
-
-const filtrar = () => {
-    const texto = filtrado.value.toLowerCase();
-    for(let personaje of filtrar){
-        let name = personaje.name.toLowerCase();
-        if(name.indexOf(texto) !== -1){
-            resultado.innerHTML +=
-            //<li>${personaje.name} - Valor: ${personaje.valor}</li>
-        //}
-    //}
-
-    //return 'filtrar';
-    console.log(filtrar);
-  };
-  
-  window.filtrar = filtrar;
-
-  boton.addEventListener('click', filtrar);
+// const filtrar = () => {
+//     const texto = filtrado.value.toLowerCase();
+//     //console.log(filtrar)
+//     for(let personaje of filtrar){
+//         let name = personaje.name.toLowerCase();
+//         if(name.indexOf(texto) !== -1){
+//         resultado.innerHTML += `<li>
+//        <img src = "${rickandmorty.img}">
+//        ${personaje.name} - Valor: ${personaje.valor}
+//        ${personaje.status} 
+//        ${personaje.gener} 
+//        ${personaje.species}
+//        </li>`
+//         }
+//     }
+// }
